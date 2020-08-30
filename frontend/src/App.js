9b69e83474c197152527ea44ad7196f32edf2174
Erica Edissa <edissa_tavares@hotmail.com>
2020-08-30 19:21:08 -0300

    Acrescentado os poup ups de alerta, a funcionalidade do botão reservar e da caixa de email

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './App.css';
 
+function reservar() {
+  
+  if (document.getElementById("email").value == "") {
+    alert("Preencha o campo \"email\" para completar a reserva!")
+  } else {
+    alert("Produto reservado!")
+  }
+
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,13 +19,13 @@ function App() {
         <div className="logoName">Minions reserva</div>
       </div>
 
-      <div className="email">e-mail <input type="email" /></div>
+      <div className="email">e-mail <input type="email" id="email" /></div>
       <div className="productsMenu">PRODUTOS - minions</div>
       <img src="minion1.png" alt="Produto minion1" className="minion1" />
 
       <div className="reserveConteiner">
           <div>produto 1 - definição</div>
-          <input type="button" value="RESERVAR"/>
+          <input type="button" value="RESERVAR" onClick={() => reservar()}/>
       </div>
     </div>
   );
